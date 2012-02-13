9c0f33e4e76705c5d6ff40b043b93ecf8d8a7f97
Isaiah Fischer <isaiah@singlebrook.com>
2012-02-13 16:52:38 -0500

    Customize registration so we can add an is 13 check box

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,15 @@ class User < ActiveRecord::Base
          :recoverable, :confirmable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :is_thirteen
+
+  validate :is_thirteen?, :on => :create
+
+  attr_accessor :is_thirteen
+
+
+  private
+  def is_thirteen?
+    errors.add(:base,'You must be at least 13 years of age to register') if is_thirteen.blank? || is_thirteen.to_i != 1
+  end
 end
