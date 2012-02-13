06ff820c4493e0b279554854f3cbc588aee3aa47
Isaiah Fischer <isaiah@singlebrook.com>
2012-02-13 09:24:05 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swidjit::Application.config.secret_token = 'c092343b9f6b5cbb1e9941127be91b64a8707012cf8a0cccfe5681dfdc4cd93881730febf03e72cf4ccbbcf0e4ca61b8088c16842767f986b4d9007ec02d4ec2'
