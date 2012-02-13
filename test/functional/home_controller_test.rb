81ad17f44e9b426c1c2a69997c20186d69785f38
Isaiah Fischer <isaiah@singlebrook.com>
2012-02-13 10:55:07 -0500

    Set up default route and a test for home controller

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can get index" do
+    get :index
+    assert_response :success
+  end
 end
