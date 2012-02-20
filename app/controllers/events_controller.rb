4b8d94ff5f956177ba5836e1914d50f3a74bdc0e
Isaiah Fischer <isaiah@singlebrook.com>
2012-02-20 08:51:59 -0500

    Ensure the start and end times are getting converted from string to actual date time

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,6 +11,8 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(params[:event])
+    @event.start_time = Event.datetimepicker_to_datetime(params[:event][:start_time])
+    @event.end_time = Event.datetimepicker_to_datetime(params[:event][:end_time])
     @event.user ||= current_user
     @event.save
     respond_with @event
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < Item
   attr_accessible :cost, :location, :start_time, :end_time
   validates_presence_of :location, :start_time, :end_time
+
+  def self.datetimepicker_to_datetime(datetime_value)
+    if datetime_value.is_a?(String)
+      Time.strptime(datetime_value,'%m/%d/%Y %l:%M %P')
+    else
+      datetime_value
+    end
+  end
 end
