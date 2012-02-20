cc945e4418ec9c6832fd76dc3255f3eaab285b07
Isaiah Fischer <isaiah@singlebrook.com>
2012-02-20 09:41:18 -0500

    Ensure we display event times in the proper time zone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :set_time_zone
+
+  def set_time_zone
+    Time.zone = 'Eastern Time (US & Canada)'
+  end
 end
--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -5,4 +5,16 @@ class EventDecorator < ItemDecorator
     event.cost || 'Free'
   end
 
+  def start_time
+    event.start_time.strftime(time_format)
+  end
+
+  def end_time
+    event.end_time.strftime(time_format)
+  end
+
+  private
+  def time_format
+    '%m/%d/%Y %l:%M %P'
+  end
 end
