629d9544d39a47df4e0da636deebcf4693a5edd6
Isaiah Fischer <isaiah@singlebrook.com>
2012-03-01 09:10:30 -0500

    User can choose from predefined conditions for a have it

--- app/models/have_it.rb
+++ app/models/have_it.rb
@@ -1,3 +1,8 @@
 class HaveIt < Item
+  CONDITIONS = ['New','Gently used','Well loved but working fine','Seen better days','Broken']
+
   attr_accessible :cost, :condition
+
+  validates_inclusion_of :condition, :in => CONDITIONS
+
 end
