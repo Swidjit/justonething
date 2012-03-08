9e985eaf73ef0be2bdd070a6c590f4c3e3337e57
sonnym <michaud.sonny@gmail.com>
2012-03-08 10:47:01 -0500

    rename and respecify user controller spec

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,18 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe UsersController do
+  describe "GET show" do
+    before(:each) { @target_user = Factory(:user) }
+
+    it "shoule be visible by guest" do
+      get :show, :display_name => @target_user.display_name
+      response.should be_success
+    end
+
+    it "should be visible by logged in user" do
+      sign_in Factory(:user)
+      get :show, :display_name => @target_user.display_name
+      response.should be_success
+    end
+  end
+end
