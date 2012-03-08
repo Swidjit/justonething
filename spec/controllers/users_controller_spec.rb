1b62ae24fb965b86e3f0b77edca608c4bcc9f407
sonnym <michaud.sonny@gmail.com>
2012-03-08 13:16:20 -0500

    typo

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,7 +4,7 @@ describe UsersController do
   describe "GET show" do
     before(:each) { @target_user = Factory(:user) }
 
-    it "shoule be visible by guest" do
+    it "should be visible by guest" do
       get :show, :display_name => @target_user.display_name
       response.should be_success
     end
