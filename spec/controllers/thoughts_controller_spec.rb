244d200d21896af2267dad70d308d40dc56938fc
Isaiah Fischer <isaiah@singlebrook.com>
2012-03-13 12:51:06 -0400

    Move thoughts cont spec into acts like item controller

--- spec/controllers/thoughts_controller_spec.rb
+++ spec/controllers/thoughts_controller_spec.rb
@@ -1,46 +1,6 @@
 require 'spec_helper'
 
 describe ThoughtsController do
-  describe "Duplicate" do
-    it 'should render new form prepopulated with specified items info' do
-      user = Factory(:user)
-      item = ThoughtDecorator.decorate Factory(:thought, :expires_on => 2.days.from_now, :tag_list => 'icecream, funstuff')
-      sign_in user
-      get :duplicate, :id => item.id
-      response.should render_template("new")
-      dup_item = assigns(:item) # This will be decorated
-      dup_item.title.should == item.title
-      dup_item.description.should == item.description
-      dup_item.tag_list.split(',').count.should == item.tags.count
-      dup_item.expires_on.should == 10.days.from_now.strftime('%m/%d/%Y')
-      dup_item.id.should_not == item.id
-    end
-  end
-
-  describe "Visibility Rules" do
-    it 'should remove the visibility rule' do
-      user = Factory(:user)
-      item = Factory(:thought)
-      list = Factory(:list, :user => user)
-      item.lists << list
-      item.list_ids.count.should == 1
-      sign_in user
-      delete :remove_visibility_rule, :id => item.id, :visibility_type => 'list', :visibility_id => list.id, :format => :json
-      response.should be_success
-      item.list_ids.count.should == 0
-    end
-
-    it 'should add the visibility rule' do
-      user = Factory(:user)
-      item = Factory(:thought)
-      list = Factory(:list, :user => user)
-      item.list_ids.count.should == 0
-      sign_in user
-      post :add_visibility_rule, :id => item.id, :visibility_type => 'list', :visibility_id => list.id, :format => :json
-      response.should be_success
-      item.list_ids.count.should == 1
-    end
-  end
 
   it_should_behave_like 'an item controller'
 end
--- spec/support/acts_like_item_controller.rb
+++ spec/support/acts_like_item_controller.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 shared_examples "an item controller" do
   render_views
 
+  before(:each) do
+    @object_sym = @controller.controller_name.singularize.to_sym
+    @object_attributes = Factory.attributes_for(@object_sym, :expires_on => 2.days.from_now.strftime('%m/%d/%Y'))
+    @object_attributes.delete(:user)
+  end
+
   describe "the new item form" do
     it "should not show the user_id select if the user is not a delagatee" do
       sign_in Factory(:user)
@@ -20,12 +26,6 @@ shared_examples "an item controller" do
   end
 
   describe "adding a new item" do
-    before(:each) do
-      @object_sym = @controller.controller_name.singularize.to_sym
-      @object_attributes = Factory.attributes_for(@object_sym, :expires_on => 2.days.from_now.strftime('%m/%d/%Y'))
-      @object_attributes.delete(:user)
-    end
-
     it "should not allow a user to set the user_id to not one of their delegates" do
       @object_attributes[:user_id] = Factory(:user).id
 
@@ -47,4 +47,46 @@ shared_examples "an item controller" do
       Item.first.posted_by_user.should == delegation.delegatee
     end
   end
+
+  describe "Duplicate" do
+    it 'should render new form prepopulated with specified items info' do
+      user = Factory(:user)
+      item = ItemDecorator.decorate Factory(@object_sym, :expires_on => 2.days.from_now, :tag_list => 'icecream, funstuff')
+      sign_in user
+      get :duplicate, :id => item.id
+      response.should render_template("new")
+      dup_item = assigns(:item) # This will be decorated
+      dup_item.title.should == item.title
+      dup_item.description.should == item.description
+      dup_item.tag_list.split(',').count.should == item.tags.count
+      dup_item.expires_on.should == 10.days.from_now.strftime('%m/%d/%Y')
+      dup_item.id.should_not == item.id
+    end
+  end
+
+  describe "Visibility Rules" do
+    it 'should remove the visibility rule' do
+      user = Factory(:user)
+      item = Factory(@object_sym)
+      list = Factory(:list, :user => user)
+      item.lists << list
+      item.list_ids.count.should == 1
+      sign_in user
+      delete :remove_visibility_rule, :id => item.id, :visibility_type => 'list', :visibility_id => list.id, :format => :json
+      response.should be_success
+      item.list_ids.count.should == 0
+    end
+
+    it 'should add the visibility rule' do
+      user = Factory(:user)
+      item = Factory(@object_sym)
+      list = Factory(:list, :user => user)
+      item.list_ids.count.should == 0
+      sign_in user
+      post :add_visibility_rule, :id => item.id, :visibility_type => 'list', :visibility_id => list.id, :format => :json
+      response.should be_success
+      item.list_ids.count.should == 1
+    end
+  end
+
 end
