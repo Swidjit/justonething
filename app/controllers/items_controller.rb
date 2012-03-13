6a685e4567913c0ced77b5dddeefab80d6a7dce0
sonnym <michaud.sonny@gmail.com>
2012-03-13 10:33:57 -0400

    encapsulate item parameter access logic

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,7 +14,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item.assign_attributes(params[item_class.to_s.underscore.to_sym])
+    @item.assign_attributes(item_params)
     format_expires_on
     @item.user ||= current_user
     @item.save
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item.assign_attributes(params[item_class.to_s.underscore.to_sym])
+    @item.assign_attributes(item_params)
     format_expires_on
     @item.save
     respond_with @item
@@ -83,11 +83,15 @@ class ItemsController < ApplicationController
 
   def format_expires_on
     if @item.has_expiration == '1' && @item.expires_on.present?
-      @item.expires_on = Date.strptime(params[item_class.to_s.underscore.to_sym][:expires_on],'%m/%d/%Y')
+      @item.expires_on = Date.strptime(item_params[:expires_on],'%m/%d/%Y')
     end
   end
 
   def item_class
     raise 'Override item_class in the child controller with the class for that type'
+  end
+
+  def item_params
+    params[item_class.to_s.underscore.to_sym]
   end
 end
