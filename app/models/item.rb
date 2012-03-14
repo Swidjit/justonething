a946537f22e0f8574448810dfc3c5ba07cf11f87
Isaiah Fischer <isaiah@singlebrook.com>
2012-03-14 11:01:31 -0400

    Keep tags in a temp array so we can unique before setting the items tags

--- app/models/item.rb
+++ app/models/item.rb
@@ -66,10 +66,12 @@ class Item < ActiveRecord::Base
 
   def convert_tag_list_to_tags
     self.tags = []
-    tag_list.split(',').map{|t| self.tags << Tag.find_or_initialize_by_name(t.strip.downcase)} if tag_list.present?
+    temp_tags = []
+    tag_list.split(',').map{|t| temp_tags << Tag.find_or_initialize_by_name(t.strip.downcase)} if tag_list.present?
     self.description.scan(/#([a-zA-Z0-9-]+)/) do |tag|
-      self.tags << Tag.find_or_initialize_by_name(tag[0].strip.downcase)
+      temp_tags << Tag.find_or_initialize_by_name(tag[0].strip.downcase)
     end
+    self.tags = temp_tags.uniq
   end
 
   def user_belongs_to_communities
