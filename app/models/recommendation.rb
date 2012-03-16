76fb6c2ec881688160430c781a58bba895b20364
Isaiah Fischer <isaiah@singlebrook.com>
2012-03-16 09:28:16 -0400

    Don't allow a user to recommend their own items

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -5,7 +5,12 @@ class Recommendation < ActiveRecord::Base
 
   validates_presence_of :user, :item, :description
   validates_uniqueness_of :user_id, :scope => :item_id
+  validate :user_cannot_recommend_own_item
 
   attr_accessible :description
+
+  def user_cannot_recommend_own_item
+    errors.add(:base, 'You cannot recommend your own item') if item.user == user
+  end
 
 end
