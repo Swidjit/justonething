d8c6ae31521a5f61287464968ff05bd3daf041ad
Isaiah Fischer <isaiah@singlebrook.com>
2012-03-16 14:16:41 -0400

    Change a few settings for tag auto complete

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,11 @@
 class TagsController < ApplicationController
 
   def autocomplete_search
-    @tags = Tag.where("name like ?", "%#{params[:q]}%")
-    if !@tags.collect(&:name).include? params[:q]
-      @tags << Tag.new(:name => params[:q])
+    new_tag = params[:q].downcase.gsub(' ','-')
+    @tags = Tag.where("name like ?", "%#{new_tag}%")
+
+    if !@tags.collect(&:name).include? new_tag
+      @tags << Tag.new(:name => new_tag)
     end
     respond_to do |format|
       format.json { render :json => @tags.collect{|tag| {:id => tag.name, :name => tag.name}} }
