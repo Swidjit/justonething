471a3e4ea4eac672ef003c117285ca24b938db60
sonnym <michaud.sonny@gmail.com>
2012-03-20 14:04:35 -0400

    method for display name suggestions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,5 +14,16 @@ describe User do
       u1.reload
       u1.display_name.should == "itsamemario"
     end
+
+    it "can be searched on" do
+      Factory(:user, :display_name => "bob")
+      Factory(:user, :display_name => "bob1")
+      Factory(:user, :display_name => "1bob1")
+      Factory(:user, :display_name => "sam")
+
+      User.lower_display_name_like('bob').all.count.should == 3
+      User.lower_display_name_like('sam').all.count.should == 1
+      User.lower_display_name_like('nil').all.count.should == 0
+    end
   end
 end
