12555e5e050bda490ddfe925ecda13559086700a
sonnym <michaud.sonny@gmail.com>
2012-03-28 16:17:38 -0400

    ability to get events based on week zero-indexed

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,11 @@ class Event < Item
   validates_presence_of :location, :start_datetime, :end_datetime, :start_date, :start_time,
     :end_date, :end_time
   attr_accessor :start_time, :start_date, :end_time, :end_date
+  
+  scope :for_week, lambda { |week| {
+    :conditions => ["#{Event.table_name}.start_datetime >= ? AND #{Event.table_name}.start_datetime <= ?",
+                    (week * 7).days.from_now.beginning_of_day, ((week + 1) * 7).days.from_now.end_of_day]
+  } }
 
   def self.datetimepicker_to_datetime(datetime_value)
     if datetime_value.is_a?(String)
@@ -12,4 +17,5 @@ class Event < Item
       datetime_value
     end
   end
+
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -28,6 +28,28 @@ describe Event do
     end
   end
 
+  describe "#for_week" do
+    it "should return an event from today with week 0" do
+      event = Factory(:event, :start_datetime => Time.now)
+      Event.for_week(0).all.should include event
+    end
+
+    it "should return an event from 7 days from now with week 0" do
+      event = Factory(:event, :start_datetime => 7.days.from_now)
+      Event.for_week(0).all.should include event
+    end
+
+    it "should not return an event from 8 days from now with week 0" do
+      event = Factory(:event, :start_datetime => 8.days.from_now)
+      Event.for_week(0).all.should_not include event
+    end
+
+    it "should return an event from 8 days from now with week 1" do
+      event = Factory(:event, :start_datetime => 8.days.from_now)
+      Event.for_week(1).all.should include event
+    end
+  end
+
   before(:all) { @item_class = Event }
   it_should_behave_like 'an item'
 end
