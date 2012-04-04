e07cd71a09ba924cfabfee9fe265cc4a481c7c71
sonnym <michaud.sonny@gmail.com>
2012-04-04 17:00:20 -0400

    factored out layout parts and enabled filtering

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -11,13 +11,15 @@ class CalendarController < ApplicationController
 private
 
   def load_events_and_preset_tags
-    day, month, year = params[:day], params[:month], params[:year]
+    day, month, year, week = params[:day], params[:month], params[:year], params[:week_no]
 
     if month.present? && day.present? && year.present?
       events = Event.for_date(Date.civil(year.to_i, month.to_i, day.to_i))
     else
-      events = Event.for_week(params[:week_no].to_i || 0)
+      events = Event.for_week(week.to_i || 0)
     end
+
+    events = events.having_tag_with_name(params[:filter]) if params[:filter].present?
 
     @events = events.all
     @user_events = current_user.present? ? events.owned_or_bookmarked_by(current_user).all : nil
