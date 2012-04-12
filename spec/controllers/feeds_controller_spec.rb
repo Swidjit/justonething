65fdc0b03851835e0a60dddcdbd1a842e3252724
Aaron Froehlich <aaron@singlebrook.com>
2012-04-12 12:21:28 -0400

    Refactoring user to have before(each)

--- spec/controllers/feeds_controller_spec.rb
+++ spec/controllers/feeds_controller_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 describe FeedsController do
+
+  before(:each) do
+    @user = Factory(:user)
+  end
+
   describe "GET all" do
     it 'should only load tagged item' do
-      user = Factory(:user)
-      item = Factory(:thought, :user => user, :tag_list => 'icecream')
-      sign_in user
+      item = Factory(:thought, :user => @user, :tag_list => 'icecream')
       get :all, :tag_name => 'icecream'
       assigns(:feed_items).should == [item]
       get :all, :tag_name => 'elephant'
@@ -15,10 +18,8 @@ describe FeedsController do
 
   describe 'GET specific item type' do
     it 'should only load items of the correct type' do
-      user = Factory(:user)
-      thought = Factory(:thought, :user => user)
-      want_it = Factory(:want_it, :user => user)
-      sign_in user
+      thought = Factory(:thought, :user => @user)
+      want_it = Factory(:want_it, :user => @user)
       get :thoughts
       assigns(:feed_items).should == [thought]
       get :want_its
@@ -28,13 +29,17 @@ describe FeedsController do
 
   describe 'GET recommendations' do
     it 'should load only items with recommendations' do
-      user = Factory(:user)
       rec = Factory(:recommendation)
       rec2 = Factory(:recommendation)
       rec3 = Factory(:recommendation, :item => rec2.item)
-      sign_in user
       get :recommendations
       assigns(:feed_items).should == [rec2.item, rec.item]
+    end
+  end
+
+  describe "GET search" do
+    it "should load items with relevant search terms" do
+      get :search
     end
   end
 end
