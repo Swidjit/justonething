51baaa2c695731ed5cd9b9f01483878af5536285
sonnym <michaud.sonny@gmail.com>
2012-04-12 17:02:44 -0400

    remove hardcoded column list

--- app/models/item.rb
+++ app/models/item.rb
@@ -62,8 +62,7 @@ class Item < ActiveRecord::Base
       controlled_scope = controlled_scope.having("COUNT(ivr.*) = 0")
     end
 
-    controlled_scope.group( %w( id title description expires_on user_id created_at updated_at type cost condition link location start_datetime end_datetime active public posted_by_user_id recommendations_count ).map{|col| "#{self.table_name}.#{col}"}.join(',')
-      ).accessible_by(ability)
+    controlled_scope.group(self.column_names.map{ |attr| "#{self.table_name}.#{attr}"}.join(",")).accessible_by(ability)
   end
 
   def set_defaults
