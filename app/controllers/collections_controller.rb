a2b73ec8929b2bb78fa4d7dd64d3a703567470c5
Isaiah Fischer <isaiah@singlebrook.com>
2012-04-16 16:54:16 -0400

    Add collections to side bar and feed to show

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,6 +1,13 @@
 class CollectionsController < ItemsController
+
+  before_filter :load_feed_items, :only => :show
+
   private
   def item_class
     Collection
+  end
+
+  def load_feed_items
+    @feed_items = @item.items
   end
 end
