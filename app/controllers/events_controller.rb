dd490c54fa017f33be3ffa615aa01169afbd958e
Isaiah Fischer <isaiah@singlebrook.com>
2012-04-23 14:42:36 -0400

    Fix event datetime formatting

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,10 +3,16 @@ class EventsController < ItemsController
 
   private
   def convert_times_to_db_format
-    start_date_time = params[:event][:start_date] + ' ' + params[:event][:start_time];
-    end_date_time = params[:event][:end_date] + ' ' + params[:event][:end_time];
-    params[:event][:start_datetime] = Event.datetimepicker_to_datetime(start_date_time)
-    params[:event][:end_datetime] = Event.datetimepicker_to_datetime(end_date_time)
+    if params[:event].present?
+      if params[:event][:start_date].present? && params[:event][:start_time].present?
+        start_date_time = params[:event][:start_date] + ' ' + params[:event][:start_time]
+        params[:event][:start_datetime] = Event.datetimepicker_to_datetime(start_date_time)
+      end
+      if params[:event][:end_date].present? && params[:event][:end_time].present?
+        end_date_time = params[:event][:end_date] + ' ' + params[:event][:end_time]
+        params[:event][:end_datetime] = Event.datetimepicker_to_datetime(end_date_time)
+      end
+    end
   end
 
   def item_class
