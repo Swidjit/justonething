dd67ac990cf64b128d8b7edf91833a4c60cc06cb
Isaiah Fischer <isaiah@singlebrook.com>
2012-04-23 15:22:01 -0400

    Psuedo Remove conditions from have its

--- app/models/have_it.rb
+++ app/models/have_it.rb
@@ -1,15 +1,15 @@
 class HaveIt < Item
-  CONDITIONS = ['New','Gently used','Well loved but working fine','Seen better days','Broken']
+#  CONDITIONS = ['New','Gently used','Well loved but working fine','Seen better days','Broken']
 
   attr_accessible :cost, :condition
 
-  validate :proper_condition
+#  validate :proper_condition
 
-  private
-  def proper_condition
-    if self.condition.present?
-      self.errors.add(:condition, 'not included in the list') if CONDITIONS.exclude?(self.condition)
-    end
-  end
+#  private
+#  def proper_condition
+#    if self.condition.present?
+#      self.errors.add(:condition, 'not included in the list') if CONDITIONS.exclude?(self.condition)
+#    end
+#  end
 
 end
