84e6b285ee9c1dfdaf248b88b7232c2c5c22eead
Isaiah Fischer <isaiah@singlebrook.com>
2012-04-23 14:21:04 -0400

    Remove underlines from @ and # links

--- lib/autoload/shared_decorations.rb
+++ lib/autoload/shared_decorations.rb
@@ -41,7 +41,7 @@ module SharedDecorations
   def linkify_profiles(text_chunk)
     new_text = text_chunk.gsub(/@([a-zA-Z0-9]+)/) do |profile_link|
       display_name = profile_link[1..-1]
-      h.link_to( "@#{display_name}", h.profile_path(:display_name => display_name) )
+      h.link_to( "@#{display_name}", h.profile_path(:display_name => display_name), :class => 'no-text-dec' )
     end
     h.sanitize new_text, :tags => 'a'
   end
@@ -49,7 +49,7 @@ module SharedDecorations
   def linkify_tags(text_chunk)
     new_text = text_chunk.gsub(/#([a-zA-Z0-9-]+)/) do |hash_tag|
       tag_name = hash_tag[1..-1]
-      h.link_to( "##{tag_name}", h.main_feeds_path(:type => 'all', :tag_name => tag_name.downcase) )
+      h.link_to( "##{tag_name}", h.main_feeds_path(:type => 'all', :tag_name => tag_name.downcase), :class => 'no-text-dec' )
     end
     h.sanitize new_text, :tags => 'a'
   end
