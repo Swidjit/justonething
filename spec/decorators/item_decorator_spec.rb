2c2d8de2fcc7f75914e95ab51dc63536b09d2e2d
sonnym <michaud.sonny@gmail.com>
2012-04-25 14:14:57 -0400

    add test for url links

--- spec/decorators/item_decorator_spec.rb
+++ spec/decorators/item_decorator_spec.rb
@@ -12,4 +12,14 @@ describe ItemDecorator do
     item.h.stub :main_feeds_path => '/feeds/all/tags'
     item.description.should == "<p>test <a href=\"/feeds/all/tags\" class=\"no-text-dec\">#tags</a> in descriptions</p>"
   end
+
+  it "should linkify urls" do
+    item = ItemDecorator.decorate Factory.build(:item, :description => "http://example.com")
+    item.description.should == '<p><a href="http://example.com">http://example.com</a></p>'
+  end
+
+  it "should not conflict between url and hashtag links" do
+    item = ItemDecorator.decorate Factory.build(:item, :description => "http://example.com/#HASHTAG")
+    item.description.should == '<p><a href="http://example.com/#HASHTAG">http://example.com/#HASHTAG</a></p>'
+  end
 end
