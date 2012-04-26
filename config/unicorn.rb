f06f290458edff56151c2c1186e60d86963da2c4
Isaiah Fischer <isaiah@singlebrook.com>
2012-04-26 16:48:05 -0400

    Take arguments to block

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,4 +2,4 @@ worker_processes 3 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
 preload_app true
 
-after_fork { ActiveRecord::Base.establish_connection }
+after_fork {|server,worker| ActiveRecord::Base.establish_connection }
