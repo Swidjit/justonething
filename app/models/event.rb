fed3362c64c62aa5d647a8deb8b8109d2914d6a3
Isaiah Fischer <isaiah@singlebrook.com>
2012-05-01 08:54:27 -0400

    Need to use the zone we provide

--- app/models/event.rb
+++ app/models/event.rb
@@ -33,7 +33,7 @@ class Event < Item
   def self.datetimepicker_to_datetime(datetime_value, time_zone)
     if datetime_value.is_a?(String) && datetime_value.present?
       Time.zone = time_zone
-      Time.strptime(datetime_value.strip,'%m/%d/%Y %l:%M %P')
+      Time.zone.strptime(datetime_value.strip,'%m/%d/%Y %l:%M %P')
     else
       datetime_value
     end
