b5eab79fe18e4ff3aec4857e66cd7e0344c4258c
sonnym <michaud.sonny@gmail.com>
2012-05-17 13:48:53 -0400

    remove debugging output

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,9 +2,7 @@ class NotificationsController < ApplicationController
   authorize_resource
 
   def index
-    Rails.logger.debug('Hitting index')
     @notifications = NotificationDecorator.decorate(current_user.notifications)
-    Rails.logger.debug @notifications.inspect
   end
 
 end
