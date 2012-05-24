a9c033ec7f08bdc1089594746d7f13f74d5120f4
sonnym <michaud.sonny@gmail.com>
2012-05-24 15:34:02 -0400

    add bcc to contact form

--- spec/controllers/contact_controller_spec.rb
+++ spec/controllers/contact_controller_spec.rb
@@ -1,11 +1,6 @@
 require 'spec_helper'
 
 describe ContactController do
-
-  before(:each) do
-    @user = Factory(:user)
-  end
-
   describe "GET index" do
     it 'should load the contact form' do
       get :new
@@ -13,14 +8,16 @@ describe ContactController do
     end
   end
 
-  describe "POST index" do
+  describe "POST create" do
     it 'should send the contact form' do
-      get :create, :subject => "Form", :message => {
+      mail_before = ActionMailer::Base.deliveries.size
+      post :create, :subject => "Form", :message => {
         :name => "Angela Groupie",
         :email => "user@swidjitgroupie.com",
         :subject => "Swidjit rocks",
         :body => "I love this service!"
       }
+      ActionMailer::Base.deliveries.size.should == mail_before + 1
       response.should redirect_to(root_path)
       flash[:notice].should =~ /message/i
     end
