e625e1378f66d3bd6b54ef4be9b14010f6788f07
David <furberd@gmail.com>
2012-07-26 22:29:15 +0300

    fix .empty? bug on feeds#index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,13 +51,17 @@ class ApplicationController < ActionController::Base
     # sometimes @feed_items is an array of objects, and does not require additional counting
     @feed_items.reset
     total_entries = @feed_items.is_a?(Array) ? @feed_items.length : Item.count_by_subquery(@feed_items)
-    @taglist = Tag.find(:all,
-                        :select => 'tags.name, count(tags.name) as tag_count',
-                        :conditions => ['items.id in (?)', @feed_items.collect(&:id)],
-                        :joins => 'INNER JOIN "items_tags" ON "items_tags"."tag_id" = "tags"."id" INNER JOIN "items" ON "items"."id" = "items_tags"."item_id"',
-                        :order => 'count(tags.name) DESC, tags.name',
-                        :limit => '20',
-                        :group => 'tags.name')
+    if @feed_items.present?
+      @taglist = Tag.find(:all,
+                          :select => 'tags.name, count(tags.name) as tag_count',
+                          :conditions => ['items.id in (?)', @feed_items.collect(&:id)],
+                          :joins => 'INNER JOIN "items_tags" ON "items_tags"."tag_id" = "tags"."id" INNER JOIN "items" ON "items"."id" = "items_tags"."item_id"',
+                          :order => 'count(tags.name) DESC, tags.name',
+                          :limit => '20',
+                          :group => 'tags.name')
+    else
+      @taglist = []
+    end
     if total_entries > 0
       @feed_items = @feed_items.paginate(:page => params[:page], :total_entries => total_entries)
       respond_to do |f|
