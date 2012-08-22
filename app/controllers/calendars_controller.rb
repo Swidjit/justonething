eaedef768ed63bff5e06c41e0878a4ed521ceb12
David <furberd@gmail.com>
2012-08-22 13:46:32 -0400

    title tag stuff

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -29,6 +29,7 @@ class CalendarsController < ApplicationController
         @filter = params[:filter]
         @calendar_title = params[:user_id].present? ? "#{@user.display_name}'s" : @current_city.name
         @calendar_title << " Events Calendar"
+        @title = @calendar_title
         @events = @calendar.events
         @user_events = @calendar.user_events
         @item_preset_tags = ItemPresetTag.where(item_type: Event.to_s)
